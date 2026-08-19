90797a3fcb9b4e954d5c3e5454285f0be5008734
N L U S Narasinghe <sawindra5339@gmail.com>
2026-08-19 09:29:33 +0530

    Fix socket.io CORS for Vercel

--- server/socket.js
+++ server/socket.js
@@ -5,7 +5,7 @@ let io;
 const initSocket = (httpServer) => {
     io = new Server(httpServer, {
         cors: {
-            origin: ["http://localhost:5173", "http://localhost:5174"], // Allow both hosts
+            origin: "*", // Allow all origins (Vercel + Local)
             methods: ["GET", "POST"]
         }
     });
