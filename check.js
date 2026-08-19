d14b176aff4a284b55801cff3155754b7257fa34
N L U S Narasinghe <sawindra5339@gmail.com>
2026-08-19 08:03:38 +0530

    Prepare for full-stack deployment (Web mock, root .gitignore, Vercel/Render ready)

--- check.js
+++
@@ -1,6 +0,0 @@
-const { Task } = require("./server/models");
-
-console.log("Checking database...");
-Task.findAll().then(tasks => {
-    console.log(JSON.stringify(tasks.map(t => ({ id: t.id, title: t.title, status: t.status })), null, 2));
-}).catch(console.error);
